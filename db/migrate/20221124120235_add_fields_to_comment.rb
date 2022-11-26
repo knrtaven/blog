cbb0d61f1e64a81801a10dd80b4ec047cc6eeef3
knrtaven <tristan.tfk@gmail.com>
2022-11-26 12:10:28 +0800

    remove add fields to comment

--- db/migrate/20221124120235_add_fields_to_comment.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToComment < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :body, :string
-    add_column :comments, :article_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_120235) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_121633) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "body"
